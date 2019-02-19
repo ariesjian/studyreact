87dac24a1d30fea44828d9a04b44c1790e353728
ariesjian <2890283899@qq.com>
2019-02-19 14:12:06 +0800

    zc

--- 01-demos/src/demos/children/index.js
+++ 01-demos/src/demos/children/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// react.children的例子
+// react.children的例子2222
 function ChildrenDemo(props) {
   console.log(props.children)
   console.log(React.Children.map(props.children, c => [c, [c, c]]));// React.Children.map返回成一层数组，得到是[children1,children1,children1,children2]
